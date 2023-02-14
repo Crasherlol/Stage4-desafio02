72601abddd174b497ce0a4a9e9164fb239d2ac68
Carlos Eduardo Coelho <89816177+Crasherlol@users.noreply.github.com>
2023-02-14 15:10:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const aluno = [
+    {
+    nome: "Carlos",
+    notaPrimeiraProva: 7,
+    notaSegundaProva: 8,
+    },
+
+    {
+    nome: "Pedro",
+    notaPrimeiraProva: 6,
+    notaSegundaProva: 9,
+    },
+
+    {nome: "Anderson",
+    notaPrimeiraProva: 8,
+    notaSegundaProva: 9,
+    },
+
+    {nome: "Ricardo",
+    notaPrimeiraProva: 3,
+    notaSegundaProva: 6,
+    },
+]
+
+function calculaMedia(notaPrimeiraProva, notaSegundaProva) {
+    const media = (notaPrimeiraProva + notaSegundaProva)/2
+    return media;
+}
+
+calculaMedia(aluno[0].notaPrimeiraProva, aluno[0].notaSegundaProva)
+
+ for (i=0; i<aluno.length; i++)
+{
+    let mediaTotal = calculaMedia(aluno[i].notaPrimeiraProva, aluno[i].notaSegundaProva)
+
+    if (mediaTotal > 7)
+    {
+        alert('A média do(a) aluno(a) '+aluno[i].nome+ ' é ' +mediaTotal+  
+        ' \nParabéns '+aluno[i].nome+ ' Você foi aprovado no concurso! ')
+
+    }
+    else 
+    {
+        alert('A média do(a) aluno(a) '+aluno[i].nome+ ' é ' +mediaTotal+' \nNão foi dessa vez '+aluno[i].nome+ ' Tente novamente! ')
+    }
+
+}
+
